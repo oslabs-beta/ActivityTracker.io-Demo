c9fbdc2354a04548435cfac0566ecdc67f60afc0
Eric DiMarzio <127116933+EricDiMarzio@users.noreply.github.com>
2024-08-22 10:09:25 -0500

    dateset

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import HeatButton from './HeatButton';
 
 function App() {
+  const [buttons, setButtons] = useState([])
+  //Assign a dataset.activty id to every button element
+  function assignIds() {
+      const everything = document.querySelectorAll("button");
+      let count = 0
+      for (const el of everything) {
+        el.dataset.activty = count;
+        count++;
+      }
+      setButtons(everything)
+      ;
+  }
+
+  function handleLogin() {
+    console.log(buttons);
+  }
+
+  useEffect(()=> {
+    assignIds();
+  },[])
+
   return (
     <div className="app">
       <h1>Activty Tracker Demo</h1>
@@ -11,7 +33,7 @@ function App() {
         <HeatButton name="Button 2" />
         <HeatButton name="Button 3" />
       </div>
-      <button className="btn" id="login">
+      <button className="btn" id="login" onClick={() => handleLogin()}>
         Login
       </button>
     </div>
